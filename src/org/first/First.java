7aa7b2fa81af82d2f5292ccb8fcb768a9bd18816
RamahAcnt <ramahravi11@gmail.com>
2021-10-22 09:09:07 +0530

    ramahtask

--- src/org/first/First.java
+++ src/org/first/First.java
@@ -1,5 +1,28 @@
 package org.first;
 
+import org.test;
+
+
 public class First {
+	
+private int sample(int a, int b, int c) {
+		
+		{
+		if(a>b && a==c);
+		{
+			System.out.println("A and C value are same");
+		}
+					
+		return b;
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		First v=new First();
+		int sample = v.sample(5, 10, 20);
+		System.out.println(sample);
+	}
+
 
 }
